7c2cfae63ac9b9c6c7b514de40b9dbf6ca8519fa
anikara <42068814+anikara@users.noreply.github.com>
2018-09-05 19:52:20 -0400

    file created2

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,30 +11,22 @@ import csv
 
 with open(fname) as f:
     csvreader = csv.reader(f)
+    next(csvreader, None)
 
     for row in csvreader:
 
-    	if (row[0] != "Date"): 
-	    	totalmonths += 1
-	    	data.append(int(row[1]))
-	    	months.append(row[0])
+	    totalmonths += 1
+	    data.append(int(row[1]))
+	    months.append(row[0])
 	    
-for i in range(1,len(data)):
-	diff.append(data[i] - data[i-1])
+
+
+diff = [data[i] - data[i-1] for i in range(1,len(data))]
 
 avgchg = (data[-1] - data[0])/(totalmonths-1)
 total_profloss = sum(data)
 
 
-
-
-# print("Financial Analysis")    	
-# print("----------------------------")
-# print(f"Total Months: {totalmonths}")
-# print(f"Total: ${total_profloss}")
-# print(f"Average  Change: ${round(avgchg,2)}")
-# print(f"Greatest Increase in Profits: {months[diff.index(max(diff))+1]} (${max(diff)})")
-# print(f"Greatest Decrease in Profits: {months[diff.index(min(diff))+1]} (${min(diff)})")
 
 f = open('PyBankResults.txt','w')
 print("Financial Analysis", file=f)    	
@@ -49,4 +41,15 @@ f.close()
 f = open('PyBankResults.txt','r')
 print(f.read())
 f.close()
+
+
+
+# print("Financial Analysis")    	
+# print("----------------------------")
+# print(f"Total Months: {totalmonths}")
+# print(f"Total: ${total_profloss}")
+# print(f"Average  Change: ${round(avgchg,2)}")
+# print(f"Greatest Increase in Profits: {months[diff.index(max(diff))+1]} (${max(diff)})")
+# print(f"Greatest Decrease in Profits: {months[diff.index(min(diff))+1]} (${min(diff)})")
+
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,14 +8,17 @@ import csv
 
 with open(fname) as f:
     csvreader = csv.reader(f)
+    next(csvreader, None)
 
     for row in csvreader:
-	    if (row[0] != "Voter ID"):
+	    
 	    	totalvote += 1
 
+	    	#increases the vote by one if its an old candidate
 	    	if (row[2]  in cands):
 	    		votebycand[cands.index(row[2])] += 1
 
+	    	#new candidate found, sets their vote to 1
 	    	else:
 	    		cands.append(row[2])
 	    		votebycand.append(1)
@@ -25,19 +28,6 @@ with open(fname) as f:
 winner = cands[votebycand.index(max(votebycand))]
 percents = [i*100/totalvote for i in votebycand]
 
-
-# print("Election Results")
-# print("-------------------------")
-# print(f"Total Votes: {totalvote}")
-# print("-------------------------")
-
-# for j in range(0,len(cands)) :
-
-# 	print(f"{cands[j]}: {round(percents[j])}.000% ({votebycand[j]})")
-
-# print("-------------------------")
-# print(f"Winner: {winner}")
-# print("-------------------------")
 
 
 
@@ -61,3 +51,19 @@ f.close()
 f = open('PyPollResults.txt','r')
 print(f.read())
 f.close()
+
+
+# print("Election Results")
+# print("-------------------------")
+# print(f"Total Votes: {totalvote}")
+# print("-------------------------")
+
+# for j in range(0,len(cands)) :
+
+# 	print(f"{cands[j]}: {round(percents[j])}.000% ({votebycand[j]})")
+
+# print("-------------------------")
+# print(f"Winner: {winner}")
+# print("-------------------------")
+
+
